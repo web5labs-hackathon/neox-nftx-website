2d50991103a6b2b2572b2fcea4ac397e8443cdf4
Zelda <3194447668@qq.com>
2024-10-02 15:34:06 +0800

    ✨ feat: about

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -4,10 +4,49 @@
 
 <template>
   <div>
-    about
+    <img class="w-full max-h-[600px] aspect-[1/2] object-cover" src="@/assets/about/cover.png" alt="">
+    <div>
+      <div class="p-container">
+        <h1>
+          Who we are？
+        </h1>
+        <p class="text-[3rem]">
+          We are Web5 Labs, a studio dedicated to building various convenient and easy-to-use software and hardware systems for people who are interested in blockchain.
+        </p>
+      </div>
+      <div class="p-container">
+        <h1>
+          Why do this project?
+        </h1>
+        <p class="text-[2.25rem]">
+          We are Web5 Labs, a studio dedicated to building various convenient and easy-to-use software and hardware systems for peoplThe launch of NEOX makes it easier to write contracts in solidity on Neo. It is a cause for celebration for the NEO community. NFT, as the core concept of crypto art, can link holders and creators with a low threshold. It can bring a large number of potential new users to the NEO community. The NFTX project can play an important role in this process.e who are interested in blockchain.
+        </p>
+      </div>
+      <div class="p-container">
+        <h1>
+          Future Outlook
+        </h1>
+        <p class="text-[2.25rem]">
+          We are Web5 Labs, a studio dedicated to building various convenient and easy-to-use software and hardware systems for peoplThe launch of NEOX makes it easier to write contracts in solidity on Neo. It is a cause for celebration for the NEO community. NFT, as the core concept of crypto art, can link holders and creators with a low threshold. It can bring a large number of potential new users to the NEO community. The NFTX project can play an important role in this process.e who are interested in blockchain.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
+.p-container {
+  @apply py-[4rem] flex flex-col space-y-6 justify-between text-center px-[2rem];
+  &:nth-child(odd) {
+    background: #f9f9fa;
+    box-shadow: -1px -2px 8px rgba(0, 0, 0, 0.125);
+  }
 
+  h1 {
+    @apply text-[4.5rem];
+  }
+  p {
+    @apply max-w-[60rem] mx-auto  text-[#000] text-opacity-60;
+  }
+}
 </style>
