6717fc8d408dc950cbf61428398746ddfcc171c0
Cyenoch <cyenoch@outlook.com>
2024-10-04 15:12:29 +0800

    connecting status

--- src/components/mint/nft.vue
+++ src/components/mint/nft.vue
@@ -2,13 +2,14 @@
 import { useAppKit } from '@reown/appkit/vue'
 import { useAccount, useWriteContract } from '@wagmi/vue'
 import { fromAsyncThrowable } from 'neverthrow'
+import { parseEther } from 'viem'
 import { toast } from 'vue-sonner'
 import { NFTX } from './NFTX'
 
 const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS
 
 const { open } = useAppKit()
-const { isConnected } = useAccount()
+const { isConnected, isConnecting, address } = useAccount()
 const { writeContractAsync, error, isPending } = useWriteContract()
 
 watch(error, (error) => {
@@ -28,22 +29,25 @@ const mintNFT = fromAsyncThrowable(async () => {
     abi: NFTX.abi,
     address: contractAddress,
     functionName: 'mint',
-    args: [BigInt(1)],
+    args: [parseEther('1')],
   })
   console.info(trxHash)
 })
 </script>
 
 <template>
-  <ElButton
-    type="primary"
-    size="large"
-    class="mx-auto !px-[1.5rem] !py-[1.25rem] !h-[3rem] !text-[1.25rem]"
-    :disabled="isPending"
-    @click="() => isConnected ? mintNFT() : open()"
-  >
-    {{ isConnected ? isPending ? 'MINTING...' : 'MINT' : 'CONNECT WALLET' }}
-  </ElButton>
+  <div class="grid place-items-center space-y-4">
+    <p>{{ address }}</p>
+    <ElButton
+      type="primary"
+      size="large"
+      class="mx-auto !px-[1.5rem] !py-[1.25rem] !h-[3rem] !text-[1.25rem]"
+      :disabled="isPending || isConnecting"
+      @click="() => isConnected ? mintNFT() : open()"
+    >
+      {{ isConnecting ? 'CONNECTING...' : isConnected ? isPending ? 'MINTING...' : 'MINT' : 'CONNECT WALLET' }}
+    </ElButton>
+  </div>
 </template>
 
 <style scoped>
