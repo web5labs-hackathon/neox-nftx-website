08c299c01e55f1efd6ec6fcfc2b823db86a80e14
Cyenoch <cyenoch@outlook.com>
2024-10-04 16:08:53 +0800

    Refactor file paths and import statements, and add switch chain functionality for minting NFTs

--- src/components/mint/nft.vue
+++ src/components/mint/nft.vue
@@ -9,9 +9,10 @@ import { NFTX } from './NFTX'
 const contractAddress = import.meta.env.VITE_CONTRACT_ADDRESS
 
 const { open } = useAppKit()
-const { isConnected, isConnecting, address } = useAccount()
+const { isConnected, isConnecting, address, chain } = useAccount()
 const { writeContractAsync, error, isPending } = useWriteContract()
 const { chains, switchChainAsync } = useSwitchChain()
+const transactionHash = ref<string>()
 
 watch(error, (error) => {
   if (error) {
@@ -26,21 +27,32 @@ watch(error, (error) => {
 })
 
 const mintNFT = fromAsyncThrowable(async () => {
+  transactionHash.value = undefined
   await switchChainAsync({ chainId: chains.value[0].id })
+  console.info('MINT')
   const trxHash = await writeContractAsync({
     abi: NFTX.abi,
     address: contractAddress,
     functionName: 'mint',
-    args: [parseEther('1')],
+    value: parseEther('0.01'),
+    args: [BigInt(1)],
   })
   toast.success(`Minting NFT... Transaction Hash: ${trxHash}`)
+  transactionHash.value = trxHash
   console.info('📔 Transaction Hash:', trxHash)
+})
+
+const explorerUrl = computed(() => {
+  const chainExplorerUrl = chain.value?.blockExplorers?.default.url
+  if (chainExplorerUrl) {
+    return `${chainExplorerUrl}/tx/${transactionHash.value}`
+  }
+  return `https://xt4scan.ngd.network/tx/${transactionHash.value}`
 })
 </script>
 
 <template>
   <div class="grid place-items-center space-y-4">
-    <p>{{ address }}</p>
     <ElButton
       type="primary"
       size="large"
@@ -50,6 +62,17 @@ const mintNFT = fromAsyncThrowable(async () => {
     >
       {{ isConnecting ? 'CONNECTING...' : isConnected ? isPending ? 'MINTING...' : 'MINT' : 'CONNECT WALLET' }}
     </ElButton>
+
+    <p v-if="address">
+      YOUR ADDRESS: {{ address }}
+    </p>
+
+    <p v-if="transactionHash" class="text-center">
+      The Most Recent Transaction Hash:<br>
+      <a class="text-blue-500 decoration-blue-600 underline" :href="explorerUrl" title="View in Explorer" target="_blank">
+        {{ transactionHash }}
+      </a>
+    </p>
   </div>
 </template>
 
